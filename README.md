7cfa2601505f66fe0557fb00656572a23cc52a15
Mahesh Ghamand <maheshghamand@gmail.com>
2021-11-20 23:04:12 +0530

    binance network support added

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 Verify Contract: 
 - `npx hardhat verify --network mainnet DEPLOYED_CONTRACT_ADDRESS "Constructor argument 1"`
 - Once the first contract is verified on etherscan, future contracts will be automatically verified by etherscan due to same bytecode
-- Auto verification is supported on: Rinkeby
+- Auto verification is supported on: 
+	- Rinkeby (ERC20_Simple, Mint, Burn, Burn_Mint) 
+	- Binance (ERC20_Simple)
--- hardhat.config.js
+++ hardhat.config.js
@@ -5,10 +5,15 @@ require('dotenv').config()
 const infuraProjectId = process.env.INFURA_PROJECT_ID
 const privateKey0 = process.env.PRIVATE_KEY_0
 const etherscanApiKey = process.env.ETHERSCAN_API_KEY
+const binanceApiKey = process.env.BINANCE_API_KEY
 
 module.exports = {
   defaultNetwork: "rinkeby",
   networks: {
+    binance: {
+      url: `https://bsc-dataseed.binance.org/`,
+      accounts: [privateKey0]
+    },
     rinkeby: {
       url: `https://rinkeby.infura.io/v3/${infuraProjectId}`,
       accounts: [privateKey0]
@@ -28,6 +33,6 @@ module.exports = {
     ]
   },
   etherscan: {
-    apiKey: etherscanApiKey
+    apiKey: binanceApiKey
   }
 }
